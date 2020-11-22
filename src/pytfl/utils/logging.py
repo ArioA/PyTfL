029f18c042e9948328108f3278375672410cc200
ArioA <ario.aliabadi@gmail.com>
2020-11-22 17:06:36 +0000

    log to stdout

--- src/pytfl/utils/logging.py
+++ src/pytfl/utils/logging.py
@@ -4,7 +4,7 @@ import logging
 import sys
 
 logging_format = "%(asctime)-15s | %(levelname)-5s | %(name)-19s | %(message)s"
-logging.basicConfig(level=logging.INFO, format=logging_format, stream=sys.stderr)
+logging.basicConfig(level=logging.INFO, format=logging_format, stream=sys.stdout)
 
 
 def getLogger(name):
